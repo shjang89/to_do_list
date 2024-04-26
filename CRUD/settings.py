71b39b3c74033dd4e27f2e44206e14df1f6b6ee7
장상호 <sangho.jang@mincoding.co.kr>
2024-04-26 16:20:44 +0900

    to_do_list_r5

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_REFERRER_POLICY = 'same-origin'
 
 # Quick-start development settings - unsuitable for production
